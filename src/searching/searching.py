9c63f326281bc654b187a052b6a7e4e0c74ee047
Ikechukwu <iykekings36@gmail.com>
2019-10-09 19:35:08 +0100

    mvp complete

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -7,14 +7,14 @@ def linear_search(arr, target):
     for i in range(len(arr)):
         if arr[i] == target:
             return i
-    return -1   # not found
+    return -1
 
 
 # STRETCH: write an iterative implementation of Binary Search
 def binary_search(arr, target):
 
     if len(arr) == 0:
-        return -1  # array empty
+        return -1
 
     low = 0
     high = len(arr) - 1
